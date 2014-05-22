2e31586b13e6c14ce9819294bb4dfb181b1ed71f
crazybach <crazybach@163.com>
2014-05-22 18:21:59 +0800

    1.3d model view script

---
+++ client/Assets/UI3DModelView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI3DModelView : MonoBehaviour {
+	
+	GameObject[] models = new GameObject[2];
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void AddModel(GameObject obj){
+		models[0] = obj;
+		//obj.transform.Equals();
+	}
+}
