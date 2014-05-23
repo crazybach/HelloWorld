9f00b2fc497e34006fb3ff9b0b46b0d5b240c46a
crazybach <crazybach@163.com>
2014-05-23 17:55:54 +0800

    1.test 3d model view, pos ,rot set by model

--- client/Assets/UI3DModelView.cs
+++ client/Assets/UI3DModelView.cs
@@ -1,23 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UI3DModelView : MonoBehaviour {
-	
-	GameObject[] models = new GameObject[2];
-	
+	//public
+	public List<GameObject> pivots = new List<GameObject>(); 
+		
+	//private
+	private List<GameObject> models = new List<GameObject>();
 	
 	// Use this for initialization
 	void Start () {
+		/*
+		model = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		model.transform.position = new Vector3(5.55f, 5.89f, 449.29f);
+		model.transform.Rotate(45f,45f,45f);
+		model.transform.localScale =new Vector3(100f,100f,100f);
+		
+		model.transform.parent = this.gameObject.transform;
+		*/
+		foreach(GameObject pivot in pivots){
+			
+			GameObject  model = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			
+			model.transform.position = pivot.transform.position;
+			model.transform.rotation = pivot.transform.rotation;
+			model.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+		
+			models.Add(model);
+		};
+		
+		/*
+		if(model == null){
+			//model = GameObject.FindGameObjectWithTag("MyCube");
+			model = GameObject.Find("Cube");
+			if(model){
+				UnityEngine.Debug.Log("Find it");
+			}
+		}
+		*/
+		
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach(GameObject model in models){
+			model.transform.Rotate(Time.deltaTime*10, 0, 0);
+		}
 	}
 	
-	void AddModel(GameObject obj){
-		models[0] = obj;
-		//obj.transform.Equals();
-	}
+	
+	
+	
 }
